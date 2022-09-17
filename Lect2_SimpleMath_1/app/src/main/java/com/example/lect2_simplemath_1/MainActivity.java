5cfda086959293888e9b012aa3126ad8869c78e0
Eren Budur <erenbudur33@gmail.com>
2022-09-16 21:23:49 -0400

    Added operations and div zero error message

--- Lect2_SimpleMath_1/app/src/main/java/com/example/lect2_simplemath_1/MainActivity.java
+++ Lect2_SimpleMath_1/app/src/main/java/com/example/lect2_simplemath_1/MainActivity.java
@@ -29,13 +29,13 @@ public class MainActivity extends AppCompatActivity {
         tvAnswer = (TextView) findViewById(R.id.tvAnswer);
         num1 = (EditText) findViewById(R.id.decimal1);
         num2 = (EditText) findViewById(R.id.decimal2);
-        addBtn  = (Button) findViewById(R.id.addButton);
-        subBtn  = (Button) findViewById(R.id.subButton);
-        mulBtn  = (Button) findViewById(R.id.mulButton);
-        divBtn  = (Button) findViewById(R.id.divButton);
-        modBtn  = (Button) findViewById(R.id.modButton);
+        addBtn = (Button) findViewById(R.id.addButton);
+        subBtn = (Button) findViewById(R.id.subButton);
+        mulBtn = (Button) findViewById(R.id.mulButton);
+        divBtn = (Button) findViewById(R.id.divButton);
+        modBtn = (Button) findViewById(R.id.modButton);
 
-        btnCalculate.setOnClickListener(new View.OnClickListener() {
+        addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Double result;
@@ -45,17 +45,57 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btnCalculate.setOnLongClickListener(new View.OnLongClickListener() {
+        subBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onLongClick(View view) {
+            public void onClick(View view) {
                 Double result;
                 result = Double.parseDouble(num1.getText().toString()) -
                         Double.parseDouble(num2.getText().toString());
                 tvAnswer.setText(result.toString());
-
-                return true;
             }
         });
+
+
+        mulBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Double result;
+                result = Double.parseDouble(num1.getText().toString()) *
+                        Double.parseDouble(num2.getText().toString());
+                tvAnswer.setText(result.toString());
+            }
+        });
+
+        divBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Double.parseDouble(num2.getText().toString()) == 0) {
+                    tvAnswer.setText("Division by 0 is not allowed");
+                    return;
+                }
+
+                Double result;
+                result = Double.parseDouble(num1.getText().toString()) /
+                        Double.parseDouble(num2.getText().toString());
+                tvAnswer.setText(result.toString());
+            }
+        });
+
+        modBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Double.parseDouble(num2.getText().toString()) == 0) {
+                    tvAnswer.setText("Mod by 0 is not allowed");
+                    return;
+                }
+
+                Double result;
+                result = Double.parseDouble(num1.getText().toString()) %
+                        Double.parseDouble(num2.getText().toString());
+                tvAnswer.setText(result.toString());
+            }
+        });
+
 
     }
 
