27a88d9c248d080f257392616ceec168fd7f061d
Eren Budur <erenbudur33@gmail.com>
2022-09-23 23:59:07 -0400

    fixed directory

---
+++ C2_P29/app/src/main/java/com/example/c2_p29/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.c2_p29;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
