65260e1dabb23fc782fa7d454fb444781db8957d
Eren Budur <erenbudur33@gmail.com>
2022-09-24 19:21:09 -0400

    Added cel and fah seekbars

--- W3_P3/app/src/main/java/com/example/w3_p3/MainActivity.java
+++ W3_P3/app/src/main/java/com/example/w3_p3/MainActivity.java
@@ -3,12 +3,51 @@ package com.example.w3_p3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    SeekBar celsius;
+    SeekBar fahrenheit;
+
+    TextView celValue;
+    TextView fahValue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        celsius = (SeekBar) findViewById(R.id.celsius);
+        fahrenheit = (SeekBar) findViewById(R.id.fahrenheit);
+        celValue = (TextView) findViewById(R.id.celVal);
+        fahValue = (TextView) findViewById(R.id.fahVal);
+
+
+        celsius.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                double celDouble = i/10;
+                double fahDouble = celDouble*1.8 +32;
+                fahValue.setText(Double.toString(fahDouble));
+                celValue.setText(Double.toString(celDouble));
+                fahrenheit.setProgress(((int)Math.round(fahDouble))  * 10 );
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
     }
 }
