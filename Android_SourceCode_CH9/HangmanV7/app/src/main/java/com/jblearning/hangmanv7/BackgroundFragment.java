b9b706f84c2d6a21e2c76f2d3b8f153f108e0831
Eren Budur <erenbudur33@gmail.com>
2022-10-22 14:28:01 -0400

    hangmanV7 without errors

---
+++ Android_SourceCode_CH9/HangmanV7/app/src/main/java/com/jblearning/hangmanv7/BackgroundFragment.java
@@ -0,0 +1,13 @@
+package com.jblearning.hangmanv7;
+
+import android.app.Fragment;
+
+public class BackgroundFragment extends Fragment {
+
+  public BackgroundFragment( ) {
+  }
+
+  public String warning( ) {
+    return "ONLY 1 LEFT!";
+  }
+}
---
+++ Android_SourceCode_CH9/HangmanV7/app/src/main/java/com/jblearning/hangmanv7/WordGame.java
@@ -0,0 +1,5 @@
+package com.jblearning.hangmanv7;
+
+public interface WordGame {
+  public abstract String currentIncompleteWord();
+}
